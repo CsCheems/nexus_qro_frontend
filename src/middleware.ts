0063f55953647875aca84123dc74ba5612e803f2
CsCheems <j.dev.mx@outlook.com>
2026-03-29 17:52:28 -0600

    se agrego middleware

---
+++ src/middleware.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest){
+    const token = request.cookies.get("token")?.value;
+
+    const { pathname } = request.nextUrl;
+
+    const protectedRoutes = [
+        "/profile"
+    ];
+
+    const isProtected = protectedRoutes.some((route) => pathname.startsWith(route));
+
+    if(isProtected && !token){
+        const loginUrl = new URL("/login", request.url);
+        return NextResponse.redirect(loginUrl);
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/profile/:path*"],
+};
